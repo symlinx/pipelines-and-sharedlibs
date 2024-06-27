adc31a8785dfe9b48ffd9fa825772d86d0243fb9
Mouloud Aouali <m@aouali.net>
2024-06-27 08:03:04 +0200

    add job name

--- init.groovy.d/pipelineAgentRouter.groovy
+++ init.groovy.d/pipelineAgentRouter.groovy
@@ -82,7 +82,7 @@ try {
                     }
                 }
             } catch (Exception e) {
-                println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] [listener: pipelineAgentRouter] [Job: ${job.name}] Error processing job: ${e.message}")
+                println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] [listener: pipelineAgentRouter] Error processing job: ${e.message}")
                 e.printStackTrace()
             }
             return null  // Allow the job to run
