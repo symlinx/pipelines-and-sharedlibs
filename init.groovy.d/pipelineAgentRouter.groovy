2f9878c9fffbd718cd963ef6fc44e36ad2691676
Mouloud Aouali <m@aouali.net>
2024-06-27 05:45:19 +0200

    vars/pipelineAgentRouterLibrary.groovy

--- init.groovy.d/pipelineAgentRouter.groovy
+++ init.groovy.d/pipelineAgentRouter.groovy
@@ -11,6 +11,9 @@ import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
 import hudson.model.queue.QueueTaskDispatcher
 import hudson.model.Queue
 import hudson.model.queue.CauseOfBlockage
+import hudson.model.Run
+import hudson.FilePath
+import hudson.model.TaskListener
 
 def instance = Jenkins.getInstance()
 
@@ -31,7 +34,7 @@ try {
         libraryConfiguration.setImplicit(true)
         libraries.add(libraryConfiguration)
         globalLibraries.setLibraries(libraries)
-        println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] Created Sharedlib pipelineAgentRouterLibrary on jenkins master")
+        println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] Created Sharedlib pipelineAgentRouterLibrary on master")
     }
 
     // Add a dispatcher to intercept pipeline execution
@@ -45,16 +48,26 @@ try {
 
                     if (definition != null) {
                         String originalPipelineScript = definition.script
-                        println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] Intercepted job by listener : ${job.name}")
+                        println("[${new Date().format('yyyy-MM-dd HH:mm:ss')}] Intercepted job: ${job.name}")
 
-                        // Check if the shared library is available
-                        def libDir = Paths.get(Jenkins.instance.getRootDir().absolutePath, 'workflow-libs', 'pipelineAgentRouterLibrary', 'vars')
-                        if (!libDir.toFile().exists()) {
-                            throw new IllegalStateException("Shared library not found in expected path: ${libDir}")
-                        }
+                        // Retrieve and validate the shared library
+                        def libRetriever = new SCMSourceRetriever(new GitSCMSource(
+                            'pipeline-agent-router-lib',
+                            'git@github.com:symlinx/pipeline-agent-router.git',
+                            'git_credential',
+                            '*',
+                            '',
+                            true
+                        ))
+                        // Use a dummy run and workspace for retrieval
+                        def dummyRun = job.getLastBuild()
+                        def dummyWorkspace = new FilePath(new File(instance.getRootDir(), "workspace/dummy"))
+                        def dummyListener = TaskListener.NULL
+
+                        libRetriever.retrieve("pipelineAgentRouterLibrary", "main", dummyWorkspace, dummyRun, dummyListener)
 
                         // Load the shared library and wrap the pipeline script
-                        def libLoader = new GroovyShell().parse(new File(libDir.toFile(), 'pipelineAgentRouterLibrary.groovy'))
+                        def libLoader = new GroovyShell().parse(new File(instance.getRootDir(), 'workflow-libs/pipelineAgentRouterLibrary/vars/pipelineAgentRouterLibrary.groovy'))
                         String modifiedPipelineScript = libLoader.call(originalPipelineScript)
 
                         definition.setScript(modifiedPipelineScript)
